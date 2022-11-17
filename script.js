86595ac66bd45d937e9f4373e1023b2d5eb81620
robertolagreca <robertolagreca92@gmail.com>
2022-11-17 14:53:33 +0100

    Array di oggetti

---
+++ script.js
@@ -0,0 +1,37 @@
+//Utilizzando i dati forniti, creare un array 
+//di oggetti per rappresentare i membri del team.
+//Ogni membro è caratterizzato dalle seguenti 
+//informazioni: nome, ruolo e foto.
+
+let arrayData = [
+    {
+        Nome: "Wayne Barnett",
+        Ruolo: "Founder & CEO",
+        Foto: "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        Nome: "Angela Caroll",
+        Ruolo: "Chief Editor",
+        Foto: "angela-caroll-chief-editor.jpg"
+    },
+    {
+        Nome: "Walter Gordon",
+        Ruolo: "Office Manager",
+        Foto: "walter-gordon-office-manager.jpg"
+    },
+    {
+        Nome: "Angela Lopez",
+        Ruolo: "Social Media Manager",
+        Foto: "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        Nome: "Scott Estrada",
+        Ruolo: "Developer",
+        Foto: "scott-estrada-developer.jpg"
+    },
+    {
+        Nome: "Barbara Ramos",
+        Ruolo: "Graphic Designer",
+        Foto: "barbara-ramos-graphic-designer.jpg"
+    }
+]
