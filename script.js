df5e1a7d5d58e7bb5db2461c8745a355c5993193
robertolagreca <robertolagreca92@gmail.com>
2022-11-17 15:24:05 +0100

    Html struttura base

--- script.js
+++ script.js
@@ -3,6 +3,14 @@
 //Ogni membro è caratterizzato dalle seguenti 
 //informazioni: nome, ruolo e foto.
 
+//MILESTONE 0:
+//Creare l’array di oggetti con le informazioni fornite.
+//MILESTONE 1:
+//Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+//MILESTONE 2:
+//Stampare le stesse informazioni su DOM sottoforma di stringhe
+
+//MILESTONE 0
 let arrayData = [
     {
         Name: "Wayne Barnett",
@@ -36,6 +44,8 @@ let arrayData = [
     }
 ]
 
+
+//MILESTONE 1
 for(const member of arrayData){
         getMember(member);
 }
@@ -49,7 +59,6 @@ function getMember (m){
     Ruolo: ${m.Role}
     Foto: ${m.Photo}
 --------------------------------------
-
-        `
+         `
     );
 }
